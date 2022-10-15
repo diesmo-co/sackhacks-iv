5c9e0cb9c2e30b641174be69b8b3960d2bff377c
YeeChuen <tychuen98@gmail.com>
2022-10-15 17:56:56 -0500

    flask skeleton

---
+++ csv reader/readcsv.py
@@ -0,0 +1,15 @@
+import csv
+
+""" 
+Author: Yee Chuen Teoh
+Title: readcsv
+Description: 
+    - queries for reading csv tables (rooms / devices / datalog)
+"""
+
+# query 1
+# get total  device energy of a room of time stamp
+
+
+# query 2
+# get total energy of a device of time stamp
