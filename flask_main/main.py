1edd2c453a225737c81f495a4d11cf304b971c0c
Zhi-Hahn See <zhihahnsee@gmail.com>
2022-10-15 19:31:50 -0700

    room + devices page functional, allows variables

--- flask_main/main.py
+++ flask_main/main.py
@@ -28,87 +28,79 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     # TODO: check the html page name
-    return render_template("homepage.html")
+    return render_template("index.html")
 
-# roompage pop up for creating new room
-@app.route("/newroom", methods=["POST","GET"])
-def newroom():
-    if request.method == "POST":
-        # new room name get the parameter using post
-        # TODO: check the variable name for the form
-        room_name = request.form["room_name"]
-        # new room id to be added to csv
-        # room_id
-        # TODO: go to csv, max(room_id)+1 will be new id
+# # roompage pop up for creating new room
+# @app.route("/newroom", methods=["POST","GET"])
+# def newroom():
+#     if request.method == "POST":
+#         # new room name get the parameter using post
+#         # TODO: check the variable name for the form
+#         room_name = request.form["room_name"]
+#         # new room id to be added to csv
+#         # room_id
+#         # TODO: go to csv, max(room_id)+1 will be new id
 
-        # TODO:pass the room_name to the roompage
-        return redirect(url_for("roompage", room_name = room_name))
-    else:
-        # TODO: check the html page name
-        return render_template("newroom.html")
+#         # TODO:pass the room_name to the roompage
+#         return redirect(url_for("roompage", room_name = room_name))
+#     else:
+#         # TODO: check the html page name
+#         return render_template("newroom.html")
 
-# below need to get some parameter to know which room to go
-@app.route("/<room_name>")
+# # below need to get some parameter to know which room to go
+@app.route("/room/<room_name>/")
 def roompage(room_name):
     # TODO: check the html page name
     # TODO: check the header variable name
     # TODO: potentially pass the graph information& list of devices
-    room = room_name
 
-    # filter the dataframe to get all the devices
-    df_devices = rooms_df.filter()
+    return render_template("room.html")
 
-    
-    # filter the dataframe devices unit categories
-    df_statistics = rooms_df.filter()
+# # devicepage pop up for creating new device
+# @app.route("/newdevice", methods=["POST","GET"])
+# def newdevice():
+#     if request.method == "POST":
+#         # new device name get the parameter using post
+#         # TODO: check the variable name for the form
+#         device_name = request.form["device_name"]
 
-    return render_template("roompage.html", room_name = room_name, df_devices = df_devices, df_statistics=df_statistics)
+#         # TODO: check the variable name for the form
+#         room_id = request.form["room_id"]
 
-# devicepage pop up for creating new device
-@app.route("/newdevice", methods=["POST","GET"])
-def newdevice():
-    if request.method == "POST":
-        # new device name get the parameter using post
-        # TODO: check the variable name for the form
-        device_name = request.form["device_name"]
+#         # TODO: check the variable name for the form
+#         device_type = request.form["device_type"]
 
-        # TODO: check the variable name for the form
-        room_id = request.form["room_id"]
-
-        # TODO: check the variable name for the form
-        device_type = request.form["device_type"]
-
-        # TODO: check the variable name for the form
-        device_kWh = request.form["device_kWh"]
+#         # TODO: check the variable name for the form
+#         device_kWh = request.form["device_kWh"]
         
 
-        # new device id to be added to csv
-        # device_id
-        # TODO: go to csv, max(device_id)+1 will be new id
+#         # new device id to be added to csv
+#         # device_id
+#         # TODO: go to csv, max(device_id)+1 will be new id
 
-        # TODO:pass the room_name to the roompage
-        return redirect(url_for("devicepage.html", device_name = device_name))
-    else:
-        # TODO: check the html page name
-        # TODO ? : pass a list of available room for the dropdown options
-        return render_template("home.html")
+#         # TODO:pass the room_name to the roompage
+#         return redirect(url_for("devicepage.html", device_name = device_name))
+#     else:
+#         # TODO: check the html page name
+#         # TODO ? : pass a list of available room for the dropdown options
+#         return render_template("home.html")
 
 
-@app.route("/<device_name>")
+@app.route("/device/<device_name>/")
 def devicepage(device_name):
-    # TODO: check the html page name
-    # TODO: check the header variable name
-    # TODO ? : potentially pass the graph & list of devices information
+#     # TODO: check the html page name
+#     # TODO: check the header variable name
+#     # TODO ? : potentially pass the graph & list of devices information
 
-    return render_template("devicepage.html", device_name=device_name)
+    return render_template("device.html")
 
-@app.route("/get<device_name>")
-def getfig(device_name):
-    # TODO: check the html page name
-    # TODO: check the header variable name
-    # TODO ? : potentially pass the graph & list of devices information
+# @app.route("/get<device_name>")
+# def getfig(device_name):
+#     # TODO: check the html page name
+#     # TODO: check the header variable name
+#     # TODO ? : potentially pass the graph & list of devices information
 
-    return ("devicepage.html", device_name=device_name)
+#     return ("devicepage.html", device_name=device_name)
 
 if __name__ == "__main__":
     app.run(debug = True)
